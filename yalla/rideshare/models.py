aee34b1ff8be0d1c0327a7e7d8cb006e81d07556
Zoe Steinkamp <zoe.steinkamp@gmail.com>
2015-11-08 16:34:47 -0700

    Fixed a few models

--- yalla/rideshare/models.py
+++ yalla/rideshare/models.py
@@ -1,14 +1,5 @@
 from django.db import models
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
 class User(models.Model):
     first_name = models.CharField(max_length=100) #Validates
     last_name = models.CharField(max_length=100)
